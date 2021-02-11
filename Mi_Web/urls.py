d575adea075d826f9ea78268dba1283d019155b2
Hector Garcia Nieves <hecgarcia.n@gmail.com>
2021-02-10 23:56:59 -0600

    Maquetacion
    -se empezo a maquetar la pagina principal

--- Mi_Web/urls.py
+++ Mi_Web/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from portafolio.views import mostrar_inicio
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mostrar_inicio,name='inicio' ),
 ]
