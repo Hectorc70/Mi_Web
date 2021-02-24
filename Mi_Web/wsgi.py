0a2f2d75e076c79289acb270c952669c70bff52a
Hector Garcia Nieves <hecgarcia.n@gmail.com>
2021-02-24 16:49:17 -0600

    Lineas de archivo de configs

--- Mi_Web/wsgi.py
+++ Mi_Web/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mi_Web.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mi_Web.settings.production')
 
 application = get_wsgi_application()
