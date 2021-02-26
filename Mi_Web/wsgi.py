031c5c6f308509d6d6a4da85a56d59dfc5a07e09
Hector Garcia Nieves <hecgarcia.n@gmail.com>
2021-02-26 11:09:17 -0600

    wsgi y manage

--- Mi_Web/wsgi.py
+++ Mi_Web/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mi_Web.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mi_Web.settings')
 
 application = get_wsgi_application()
