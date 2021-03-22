978bf2711871d75a14a0e537406871219855dcec
Hector Garcia Nieves <hecgarcia.n@gmail.com>
2021-03-22 16:40:58 -0600

    Responsive Design se creo el responsive design se creo nuevo modelo para lo que es la foto del portafolio, link del cv se agregaron dos campos nuevos a el modelo de proyecto, para poder poner los links del demo y codigo

--- Mi_Web/settings.py
+++ Mi_Web/settings.py
@@ -106,8 +106,8 @@ DATABASES = {
             'PORT': config['TEST']['DB_PORT'],
         }
 } 
- """
 
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- portafolio/models.py
+++ portafolio/models.py
@@ -2,13 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+class Profile(models.Model):
+    id_profile = models.AutoField(primary_key=True,
+                                    auto_created=True, unique=True) 
+    name         = models.CharField('Nombre', max_length=300)
+    description  = models.TextField('Descripcion', blank=True)    
+    photo_profile = models.CharField('Imagen de perfil', blank=True, max_length=500)
+    link_cv      = models.CharField('Path CV', max_length= 500, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Project(models.Model):
-    id_project  = models.AutoField(primary_key=True,
+    id_project   = models.AutoField(primary_key=True,
                                     auto_created=True, unique=True) 
     name         = models.CharField('Nombre de Proyecto', max_length=300)
     description  = models.TextField('Descripcion del Proyecto')
     picture_head = models.CharField('Imagen', max_length=500)
+    code_link    = models.CharField('link codigo', max_length=500,blank=True) 
+    demo_link    = models.CharField('link demo', max_length=500,blank=True) 
 
     def __str__(self):
         return self.name
